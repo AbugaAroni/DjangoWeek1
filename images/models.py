9b0e465848ec4561c58fcbd24991b5515a1a3f78
AbugaAroni <abugaaroni@gmail.com>
2020-10-13 17:26:25 +0300

    add editor test cases

--- images/models.py
+++ images/models.py
@@ -20,6 +20,9 @@ class Editor(models.Model):
         editors = Editor.objects.all()
         return editors
 
+    def update_editor(self):
+        Editor.objects.filter(first_name = self.first_name).update(first_name ='rick')
+
     class Meta:
         ordering = ['first_name']
 
@@ -36,6 +39,20 @@ class Categories(models.Model):
     def __str__(self):
         return self.category_name
 
+    def save_cat(self):
+        self.save()
+
+    def delete_cat(self):
+        self.delete()
+
+    def update_cat(self):
+        categories.objects.filter(category_name = self.category_name).update(category_name ='rick')
+
+    def display_cat():
+        catagories = categories.objects.all()
+        return catagories
+
+
 class Images(models.Model):
     _images = models.ImageField(upload_to = 'images/')
     title = models.CharField(max_length =60)
@@ -45,3 +62,9 @@ class Images(models.Model):
 
     def __str__(self):
         return self.title
+
+     def save_image(self):
+         self.save()
+
+     def delete_image(self):
+         self.delete()
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,93 @@
 from django.test import TestCase
+from .models import Editor,Location,Categories,Images
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'Abuga', last_name ='Aroni', email ='abugaaroni@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abuga,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.abuga.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.abuga.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+        self.abuga.delete_editor()
+        editors = Editor.objects.filter(first_name='Abuga')
+        self.assertEqual(len(editors), 0)
+
+    #Testing display method
+    def test_display_method(self):
+        self.abuga.save_editor()
+        goat = Editor.display_editor()
+        editors = Editor.objects.all()
+
+        self.assertTrue(len(editors), len(goat))
+
+    # Testing update Method
+    def test_update_method(self):
+        self.abuga.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+        self.abuga.update_editor()
+        editors = Editor.objects.filter(first_name='rick')
+        self.assertTrue(len(editors) > 0)
+
+class CategoriesTestClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.abugacat= Categories(category_name = 'Mombasa')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abugacat,Categories))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.abugacat.save_cat()
+        cats = Categories.objects.all()
+        self.assertTrue(len(cats) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.abugacat.save_cat()
+        cats = Categories.objects.all()
+        self.assertTrue(len(cats) > 0)
+
+        self.abugacat.delete_cat()
+        cats = Categories.objects.filter(category_name='Mombasa')
+        self.assertEqual(len(cats), 0)
+
+    #Testing display method
+    def test_display_method(self):
+        self.abugacat.save_cat()
+        goat = Categories.display_cat()
+        catz = Categories.objects.all()
+        self.assertTrue(len(catz), len(goat))
+
+    # Testing update Method
+    def test_update_method(self):
+        self.jamestag.save_tag()
+        editors = tags.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+        self.jamestag.update_tag()
+        editors = tags.objects.filter(name='rick')
+        self.assertTrue(len(editors) > 0)
+
+
+
+#class ImagesTestClass(TestCase):
