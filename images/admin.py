f457206615db70994473a4940eef33503723f5a0
AbugaAroni <abugaaroni@gmail.com>
2020-10-14 13:39:23 +0300

    add admin stuff and view images html and relevant views fucntions

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Editor,Locations,Categories,Images
 # Register your models here.
+
+
+admin.site.register(Editor)
+admin.site.register(Images)
+admin.site.register(Locations)
+admin.site.register(Categories)
