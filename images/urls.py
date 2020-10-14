4d010d691b44c6ae43916c2926bf49a39445d426
AbugaAroni <abugaaroni@gmail.com>
2020-10-14 16:18:07 +0300

    add view and template to view single picture and details

--- images/urls.py
+++ images/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',views.welcome, name='Welcome'),
     re_path('search/', views.search_results, name='search_results'),
+    re_path('singleimage/(\d+)',views.singleimage,name ='View image')    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Locations,Categories,Blog_Images
 
 # Create your views here.
@@ -21,3 +21,10 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'everything/search.html',{"message":message})
+
+def singleimage(request,image_id):
+    try:
+        imagez = Blog_Images.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'everything/single-picture.html', {"imagez":imagez})
